19c48561fd0625098762e134b81c876155fa6c3e
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2022-07-01 20:05:52 +0530

    Update python.py

--- reverse an array/python.py
+++ reverse an array/python.py
@@ -1,1 +1,14 @@
+------------------------------     METHOD-1      ----------------------------------------------------
+def reverseList(A, start, end):
+	while start < end:
+		A[start], A[end] = A[end], A[start]
+		start += 1
+		end -= 1
+A = [1, 2, 3, 4, 5, 6]
+reverseList(A, 0, 5)
+print("Reversed list is" ,A)
 
+------------------------------     METHOD-2      ----------------------------------------------------
+
+A = [1, 2, 3, 4, 5, 6]
+print( A[::-1])
